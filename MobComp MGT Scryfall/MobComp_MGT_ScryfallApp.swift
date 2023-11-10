e1d371d28adb6437b859c991657579766545ab05
Angeline Ivana <angelineivana01@student.ciputra.ac.id>
2023-11-10 08:45:20 +0700

    Initial Commit

---
+++ MobComp MGT Scryfall/MobComp_MGT_ScryfallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobComp_MGT_ScryfallApp.swift
+//  MobComp MGT Scryfall
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MobComp_MGT_ScryfallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
