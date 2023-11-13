161966ce9201421a7463ccd05f9b58f8dc448540
angelineivana <95264796+angelineivana@users.noreply.github.com>
2023-11-13 16:30:27 +0700

    Add 'VERSION' and 'RULING' button (pricing and legalities)

--- MobComp MGT Scryfall/MobComp MGT Scryfall/ContentView.swift
+++ MobComp MGT Scryfall/MobComp MGT Scryfall/ContentView.swift
@@ -24,6 +24,9 @@ struct SearchBar: View {
 
 struct MTGCardView: View {
     var card: MTGCard
+    @State private var showVersion = false
+    @State private var showRuling = false
+    @State private var pricingInfo: [CardPricingInfo] = []
     
     var body: some View {
         VStack {
@@ -34,6 +37,7 @@ struct MTGCardView: View {
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
                 case .failure:
                     Image(systemName: "exclamationmark.triangle")
                         .resizable()
@@ -49,17 +53,250 @@ struct MTGCardView: View {
             // Tampilkan nama kartu
             Text(card.name)
                 .font(.title)
-                .padding()
-            
-            // Tampilkan jenis kartu dan teks orakel wrapped in a frame
+//                .padding()
+            Text(card.type_line)
+
             FrameView {
-                Text("Type: \(card.type_line)")
-                Text("Oracle Text: \(card.oracle_text)")
+                VStack {
+                    Text(card.oracle_text)
+//                        .padding(.trailing)
+                        .background(Color.clear)
+                    Text(card.flavor_text ?? "")
+//                        .padding(.trailing)
+                        .background(Color.clear)
+                }
+            }
+            
+            HStack {
+                Button(action: {
+                    fetchPricingAndLegalities(for: card)
+                    //                    self.showVersion.toggle()
+                }) {
+                    Text("Versions")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(30)
+                }
+                .sheet(isPresented: $showVersion) {
+                    CardPricingListView(pricingInfo: pricingInfo)
+                }
+                .onChange(of: pricingInfo) { _, newPricingInfo in
+                    if !newPricingInfo.isEmpty {
+                        self.showVersion.toggle()
+                    }
+                }
+                
+                Button(action: {
+                    self.showRuling.toggle()
+                }) {
+                    Text("Ruling")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(30)
+                }
+                .sheet(isPresented: $showRuling) {
+                    LegalitiesView(legalities: card.legalities)
+                }
             }
             .padding()
         }
     }
+    
+    struct CardPricingInfo: Codable, Equatable {
+        var name: String
+        var set: String
+        var rarity: String
+        var language: String
+        var prices: Prices
+
+        struct Prices: Codable, Equatable {
+            var usd: String
+            var usd_foil: String?
+            var eur: String
+            var eur_foil: String?
+            var tix: String
+        }
+
+        static func == (lhs: CardPricingInfo, rhs: CardPricingInfo) -> Bool {
+            return lhs.name == rhs.name &&
+                   lhs.set == rhs.set &&
+                   lhs.rarity == rhs.rarity &&
+                   lhs.language == rhs.language &&
+                   lhs.prices == rhs.prices
+        }
+    }
+
+    struct CardPricingListView: View {
+        var pricingInfo: [CardPricingInfo]
+
+        var body: some View {
+            Text("Pricing Information")
+                .font(.system(size: 20, weight: .semibold))
+                .padding(.top, 24)
+            VStack {
+                List(pricingInfo, id: \.name) { info in
+                    VStack(alignment: .leading) {
+                        Text("\(info.set): \(info.name)")
+                            .font(.headline)
+
+                        Text("#1 · \(info.rarity) · \(info.language) · Nonfoil/Foil")
+                            .foregroundColor(.gray)
+                        HStack {
+                            Text("USD: \(info.prices.usd)")
+                            Spacer()
+                            Text("EUR: \(info.prices.eur)")
+                            Spacer()
+                            Text("TIX: \(info.prices.tix)")
+                        }
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .padding(.vertical, 8)
+                }
+                .padding()
+            }
+        }
+    }
+
+    func fetchPricingAndLegalities(for card: MTGCard) {
+        pricingInfo = [
+            CardPricingInfo(
+                name: card.name,
+                set: card.set_name ?? "",
+                rarity: card.rarity ?? "",
+                language: card.lang ?? "",
+                prices: CardPricingInfo.Prices(
+                    usd: card.prices?.usd ?? "",
+                    usd_foil: card.prices?.usd_foil,
+                    eur: card.prices?.eur ?? "",
+                    eur_foil: card.prices?.eur_foil,
+                    tix: card.prices?.tix ?? ""
+                )
+            )
+        ]
+    }
+    
+    struct LegalitiesView: View {
+        var legalities: MTGCard.Legality?
+
+        var body: some View {
+            VStack(alignment: .leading, spacing: 8) {
+                Text("LEGALITIES")
+                    .foregroundColor(.red)
+                    .font(.system(size: 20, weight: .bold))
+                .padding(.horizontal)
+                
+                ScrollView {
+                    if let standard = legalities?.standard {
+                        LegalitiesRow(format: "Standard", legality: standard)
+                    }
+                    if let future = legalities?.future {
+                        LegalitiesRow(format: "Future", legality: future)
+                    }
+                    if let historic = legalities?.historic {
+                        LegalitiesRow(format: "Historic", legality: historic)
+                    }
+                    if let gladiator = legalities?.gladiator {
+                        LegalitiesRow(format: "Gladiator", legality: gladiator)
+                    }
+                    if let pioneer = legalities?.pioneer {
+                        LegalitiesRow(format: "Pioneer", legality: pioneer)
+                    }
+                    if let explorer = legalities?.explorer {
+                        LegalitiesRow(format: "Explorer", legality: explorer)
+                    }
+                    if let modern = legalities?.modern {
+                        LegalitiesRow(format: "Modern", legality: modern)
+                    }
+                    if let legacy = legalities?.legacy {
+                        LegalitiesRow(format: "Legacy", legality: legacy)
+                    }
+                    if let pauper = legalities?.pauper {
+                        LegalitiesRow(format: "Pauper", legality: pauper)
+                    }
+                    if let vintage = legalities?.vintage {
+                        LegalitiesRow(format: "Vintage", legality: vintage)
+                    }
+                    if let penny = legalities?.penny {
+                        LegalitiesRow(format: "Penny", legality: penny)
+                    }
+                    if let commander = legalities?.commander {
+                        LegalitiesRow(format: "Commander", legality: commander)
+                    }
+                    if let oathbreaker = legalities?.oathbreaker {
+                        LegalitiesRow(format: "Oathbreaker", legality: oathbreaker)
+                    }
+                    if let brawl = legalities?.brawl {
+                        LegalitiesRow(format: "Brawl", legality: brawl)
+                    }
+                    if let historicbrawl = legalities?.historicbrawl {
+                        LegalitiesRow(format: "Historic Brawl", legality: historicbrawl)
+                    }
+                    if let alchemy = legalities?.alchemy {
+                        LegalitiesRow(format: "Alchemy", legality: alchemy)
+                    }
+                    if let paupercommander = legalities?.paupercommander {
+                        LegalitiesRow(format: "Pauper Commander", legality: paupercommander)
+                    }
+                    if let duel = legalities?.duel {
+                        LegalitiesRow(format: "Duel", legality: duel)
+                    }
+                    if let oldschool = legalities?.oldschool {
+                        LegalitiesRow(format: "Old School", legality: oldschool)
+                    }
+                    if let premodern = legalities?.premodern {
+                        LegalitiesRow(format: "Premodern", legality: premodern)
+                    }
+                    if let predh = legalities?.predh {
+                        LegalitiesRow(format: "Prismatic", legality: predh)
+                    }
+                }
+                
+            }
+            .padding()
+        }
+    }
+
+    struct LegalitiesRow: View {
+        var format: String
+        var legality: String
+
+        var body: some View {
+            HStack {
+                Text(legality == "legal" ? "LEGAL" : "NOT LEGAL")
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
+                    .background(legality == "legal" ? Color.green : Color.gray)
+                    .cornerRadius(8)
+                Spacer()
+                Text(format)
+            }
+            .font(.system(size: 15, weight: .semibold))
+            .padding(.horizontal)
+        }
+    }
+
+
 }
+
+
+
+//struct ExplanationView: View {
+//    var explanation: String
+//    var title: String
+//
+//    var body: some View {
+//        // Customize the appearance of the explanation view
+//        VStack {
+//            Text("Test")
+//            Spacer()
+//        }
+//    }
+//}
 
 struct FrameView<Content: View>: View {
     let content: Content
@@ -83,7 +320,7 @@ struct ContentView: View {
     @State private var isSortingAscending = true
 
 
-    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3) // Three cards per row
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
 
     var body: some View {
         TabView {
@@ -92,13 +329,14 @@ struct ContentView: View {
                     HStack {
                         // Search Bar
                         SearchBar(searchText: $searchText)
-                            .padding()
+//                            .padding()
 
                         // Sort Button
                         Button(action: {
                             isSortingAscending.toggle()
                             sortMTGCardsByName()
                         }) {
+                            Text("Sort")
                             Image(systemName: isSortingAscending ? "arrow.up" : "arrow.down")
                                 .imageScale(.large)
                                 .padding()
@@ -110,36 +348,32 @@ struct ContentView: View {
                         }) { card in
                             NavigationLink(destination: MTGCardView(card: card)) {
                                 CardImageView(card: card)
-                                    .frame(height: 300) // Adjust the image height as needed
+                                    .frame(height: 200) // Adjust the image height as needed
                             }
                         }
                     }
                     .padding()
                 }
                 .onAppear {
-                    // Load data from a JSON file
                     if let data = loadJSON() {
                         do {
                             let decoder = JSONDecoder()
                             let cards = try decoder.decode(MTGCardList.self, from: data)
                             mtgCards = cards.data
+                            print("Data loaded successfully: \(mtgCards)")
                         } catch {
                             print("Error decoding JSON: \(error)")
                         }
+                    } else {
+                        print("Failed to load JSON data")
                     }
                 }
+
                 .navigationBarTitle("MTG Cards")
             }
-            .tabItem {
-                Image(systemName: "house")
-                Text("Home")
-            }
-
-          
         }
     }
 
-    // Function to load data from a JSON file
     func loadJSON() -> Data? {
         if let path = Bundle.main.path(forResource: "WOT-Scryfall", ofType: "json") {
             do {
@@ -174,10 +408,9 @@ struct CardImageView: View {
             RoundedRectangle(cornerRadius: 8)
                 .fill(Color.white)
                 .shadow(radius: 3)
-                .frame(height: 200) // Adjust the frame height
+                .frame(height: 200)
             
             VStack {
-                // Card image
                 AsyncImage(url: URL(string: card.image_uris?.large ?? "")) { phase in
                     switch phase {
                     case .success(let image):
@@ -196,10 +429,9 @@ struct CardImageView: View {
                     }
                 }
                 
-                // Text under the image
                 Text(card.name)
-                    .font(.system(size: 14)) // Adjust the size (e.g., 14) to your preference
-                    .padding(.top, 8) // Adjust the padding as needed
+                    .font(.system(size: 14))
+                    .padding(.top, 8)
             }
         }
     }
--- MobComp MGT Scryfall/MobComp MGT Scryfall/MGTCard.swift
+++ MobComp MGT Scryfall/MobComp MGT Scryfall/MGTCard.swift
@@ -7,14 +7,50 @@ struct MTGCard: Codable, Identifiable {
     var type_line: String
     var oracle_text: String
     var image_uris: ImageURIs?
+    var flavor_text: String?
+    var set_name: String?
+    var rarity: String?
+    var lang: String?
+    var prices: Prices?
+    var legalities: Legality?
 
-    // Define other properties as needed based on your JSON structure
+    struct Prices: Codable {
+        var usd: String?
+        var usd_foil: String?
+        var usd_etched: String?  
+        var eur: String?
+        var eur_foil: String?
+        var tix: String?
+    }
+    
+    struct Legality: Codable {
+        var standard: String?
+        var future: String?
+        var gladiator: String?
+        var pioneer: String?
+        var modern: String?
+        var legacy: String?
+        var vintage: String?
+        var commander: String?
+        var oathbreaker: String?
+        var alchemy: String?
+        var explorer: String?
+        var brawl: String?
+        var historic: String?
+        var historicbrawl: String?
+        var pauper: String?
+        var paupercommander: String?
+        var penny: String?
+        var duel: String?
+        var oldschool: String?
+        var predh: String?
+        var premodern: String?
+    }
 
     struct ImageURIs: Codable {
         var small: String?
         var normal: String?
         var large: String?
-        // Add other image URL properties if needed
     }
 }
 
